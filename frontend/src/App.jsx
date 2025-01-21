a3a461dd455e50068621803475f9e9e52cb6d351
krish21-j <krish.jain.s64@kalvium.community>
2025-01-21 13:42:09 +0530

    login

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { LoginPage } from "./routes";
+
+import './App.css';
+
+function App() {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/login" element={<LoginPage />} />
+   </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
